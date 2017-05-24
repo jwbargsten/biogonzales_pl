12ef8e054a5e57a5fcf83e554bb9fe9c2b7e8dd3
Joachim Bargsten <jw@bargsten.org>
2017-05-24 14:20:09 +0200

    fixed a bug in geno2haplo

--- lib/Bio/Gonzales/Var/Util.pm
+++ lib/Bio/Gonzales/Var/Util.pm
@@ -15,15 +15,20 @@ our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
 
 sub geno2haplo {
   my $genotypes = shift;
+  my $ploidy = shift;
+
+  die "no ploidy defined for geno2haplo" unless(defined($ploidy));
   # check if also coverage, etc. is part of the genotype then
   # split the genotypes into haplotypes
-  my @haplotypes;
   my $phased = 1;
+  my @haplotypes;
   for my $g_raw (@$genotypes) {
     my $g = index( $g_raw, ':' ) >= 0 ? substr( $g_raw, 0, index( $g_raw, ':' ) ) : $g_raw;
     # we need to find only one genotype of x/y to set phased to false
     $phased &&= not index( $g, '|' ) < 0;
-    push @haplotypes, split /[|\/]/, $g;
+    my @h = split /[|\/]/, $g;
+    @h = ('.')x$ploidy if(@h == 1 && $h[0] eq '.');
+    push @haplotypes, @h;
   }
   return ( \@haplotypes, $phased );
 }
