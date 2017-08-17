4bdcc25abfdce87e8d78ba66b634ea5b7540e3c8
Joachim Bargsten <jw@bargsten.org>
2017-08-17 19:55:36 +0200

    working on summarizedexperiment

--- lib/Bio/Gonzales/SummarizedExperiment.pm
+++ lib/Bio/Gonzales/SummarizedExperiment.pm
@@ -21,34 +21,79 @@ has [qw/assay col_data row_data row_names col_names/] => ( is => 'rw', default =
 sub data { shift->assay(@_) };
 sub header { shift->col_names(@_) };
 
-sub row_idx {
-  my ($self, $name) = @_;
-
-  firstidx { $_ eq $name } @{$self->row_names};
+sub _idx {
+  my ($self, $m, $name) = @_;
+  firstidx { $_ eq $name } @{$self->$m};
 }
 
-sub col_idx {
-  my ($self, $name) = @_;
+sub row_idx { shift->_idx('row_names', @_) }
 
-  firstidx { $_ eq $name } @{$self->col_names};
-}
+sub col_idx { shift->_idx('col_names', @_) }
 
 sub header_idx { shift->col_idx(@_) }
 
-sub row_idx_match {
-  my ($self, $rex) = @_;
 
-  [ indexes { $_ =~ /$rex/ } @{$self->row_names} ]
+sub _idx_match {
+  my ($self, $m, $rex) = @_;
+  [ indexes { $_ =~ /$rex/ } @{$self->$m} ]
 }
 
-sub col_idx_match {
-  my ($self, $rex) = @_;
+sub row_idx_match { shift->_idx_match('row_names', @_) }
 
-  [ indexes { $_ =~ /$rex/ } @{$self->col_names} ]
-}
+sub col_idx_match { shift->_idx_match('col_names', @_) }
 
 sub header_idx_match {
   shift->col_idx_match(@_);
+}
+
+
+sub add_cols {
+  my ($self, $cols, $names) = @_;
+
+  push @{$self->col_names}, $name if(defined($name));
+
+  my $assay = $self->assay;
+  die unless(@$col == @$assay);
+
+  # FIXME col kein eine fkt sein
+  for(my $i = 0; $i < @$assay; $i++) {
+    push @{$assay->[$i]}, $col->[$i];
+  }
+
+  return $self;
+}
+
+sub add_rows {
+  my ($self, $rows, $names) = @_;
+
+  push @{$self->row_names}, $name if(defined($name));
+
+  push @{$self->assay}, $row;
+  return $self;
+}
+
+sub row_ref_apply {
+
+}
+
+sub col_apply {
+
+}
+
+sub aggregate_by_idcs {
+  my ($self, $idcs, $fun, $col_names) = @_;
+
+  sub {
+    my ( $data,  $row_idcs,$col_idcs, $row_names, $col_names) = @_;
+    return ($row, $row_name);
+  }
+  
+  return 
+
+}
+
+sub aggregate_by_names {
+
 }
 
 1;
