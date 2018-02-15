12c93390bfb9611f3c12f35b4d1c1cf2fbd3e98b
Joachim Bargsten <jw@bargsten.org>
2018-02-15 09:46:06 +0100

    added chunk processing for cli.md files and added functions to var util

--- lib/Bio/Gonzales/Var/Util.pm
+++ lib/Bio/Gonzales/Var/Util.pm
@@ -8,9 +8,12 @@ use 5.010;
 
 use Exporter 'import';
 
+use List::MoreUtils qw/uniq/;
+use List::Util qw/max/;
+
 our $VERSION = 0.01_01;
 
-our %EXPORT_TAGS = ( 'all' => [qw/geno2haplo renumber_genotypes merge_alleles only_geno/ ] );
+our %EXPORT_TAGS = ( 'all' => [qw/geno2haplo renumber_genotypes merge_alleles geno_get_gt_simple/ ] );
 our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
 
 sub geno2haplo {
@@ -32,11 +35,6 @@ sub geno2haplo {
     push @haplotypes, @h;
   }
   return ( \@haplotypes, $phased );
-}
-
-sub only_geno {
-  my $genotypes = shift;
-  my @res = map { index( $_, ':' ) >= 0 ? substr( $_, 0, index( $_, ':' ) ) : $_ } @$genotypes;
 }
 
 sub renumber_genotypes {
@@ -77,5 +75,93 @@ sub merge_alleles {
   }
   return ( \@merged_alleles, \@map );
 }
+
+sub geno_get_gt_simple {
+  my $genotypes = shift;
+  my @res = map { index( $_, ':' ) >= 0 ? substr( $_, 0, index( $_, ':' ) ) : $_ } @$genotypes;
+  return \@res;
+}
+
+sub geno_get_gt {
+  my ( $gt, $smp_idcs, $gt_idx ) = @_;
+  $gt_idx //= 0;
+
+  my @gt;
+  $smp_idcs = [ 0 .. (@$gt -1 )] unless($smp_idcs);
+  for my $i (@$smp_idcs) {
+    my $call = $gt->[$i];
+
+    my @fields = split /:/, $call;
+    if ( index( $fields[$gt_idx], '.' ) >= 0 ) {
+      push @gt, '.';
+    } else {
+      push @gt, $fields[$gt_idx];
+    }
+  }
+  return \@gt;
+}
+
+sub geno_count_gt {
+  my $gt = shift;
+
+  my %cnt;
+  map { $cnt{$_}++ } @$gt;
+  return \%cnt;
+}
+
+sub geno_nhet {
+  my $gt = shift;
+
+  my $nhet = 0;
+  for my $g (@$gt) {
+    my @called_alleles = split /[\/|]/, $g;
+    $nhet++ if ( uniq(@called_alleles) > 1 );
+
+  }
+  return $nhet;
+}
+
+sub geno_is_poly {
+  my $gt = shift;
+  my @valid_gt = grep { $_ ne '.' } @$gt;
+  return 0 unless (@valid_gt);
+  return uniq(@valid_gt) == 1 ? 0 : 1;
+}
+
+sub geno_split_hom_het {
+  my $gt = shift;
+  my @hom;
+  my @het;
+  my $nmissing = 0;
+
+  for my $g (@$gt) {
+    if ( $g eq '.' ) {
+      $nmissing++;
+      next;
+    }
+    my @called_alleles = split /[\/|]/, $g;
+    if ( uniq(@called_alleles) > 1 ) {
+      push @het, $g;
+    } else {
+      push @hom, $g;
+    }
+  }
+  return ( \@hom, \@het, $nmissing );
+}
+
+sub geno_nmissing {
+  my $gt = shift;
+  return scalar grep { $_ eq '.' } @$gt;
+}
+
+sub geno_major_alleles {
+  my $cnt = shift;
+
+  my @valid         = grep { $_ ne '.' } keys %$cnt;
+  my $cnt_major     = max @{$cnt}{@valid};
+  my @major_alleles = grep { $cnt->{$_} == $cnt_major && $_ ne '.' } @valid;
+  return \@major_alleles;
+}
+
 
 1;
